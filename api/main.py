6f417f1ea10e74dfb99675f2a4156ceebda7e4b1
m-oka-system <m-oka-system@outlook.com>
2025-09-05 15:07:21 +0900

    CORS設定にlocalhost:8081を追加

--- api/main.py
+++ api/main.py
@@ -14,6 +14,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:8081",
+        "http://127.0.0.1:8081",
         "https://*.azurewebsites.net",
         "http://*.azurewebsites.net"
     ],
